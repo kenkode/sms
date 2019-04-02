1d93b9f1c7f0767b98e2cdcaab53cc943f0bf0e6
kenkode <wangoken2@gmail.com>
2019-04-02 09:33:16 +0300

    sms

---
+++ database/migrations/2019_04_02_062020_create_vacation_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('vacation_type_id');
+            $table->string('type');
+            $table->integer('requested_days');
+            $table->string('status');
+            $table->string('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacations');
+    }
+}
